687545814bf3e50bddac3cf87ac4fa2420126397
Ramela M S <ramelams07@gmail.com>
2025-12-10 05:32:20 +0530

    Fix clean requirements for Render

--- eta/__init__.py
+++ eta/__init__.py
@@ -1,16 +1,17 @@
 from flask import Flask
 from flask_login import LoginManager
-
+from flask_sqlalchemy import SQLAlchemy
+import os
 
 app= Flask(__name__)
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = "login_page"
 
-from flask_sqlalchemy import SQLAlchemy
-app.config['SECRET_KEY'] = 'your_secret_key_here'
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY", "your_secret_key_here")
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///instance/database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db=SQLAlchemy(app)
